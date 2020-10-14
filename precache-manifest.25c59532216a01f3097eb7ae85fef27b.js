ffbff41fd936823d1c3151ef6974faa2c96c0704
Amisha <amisha@bigscal.com>
2020-10-14 12:19:08 -0700

    Updates

---
+++ precache-manifest.25c59532216a01f3097eb7ae85fef27b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e23fdafbaaa77a16a1b295465ba09474",
+    "url": "/enegrysemulator.ml/index.html"
+  },
+  {
+    "revision": "bf6023511303dcf03f77",
+    "url": "/enegrysemulator.ml/static/css/main.dfb176b5.chunk.css"
+  },
+  {
+    "revision": "12729295abd33b6190cc",
+    "url": "/enegrysemulator.ml/static/js/2.a5b30c2d.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/enegrysemulator.ml/static/js/2.a5b30c2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf6023511303dcf03f77",
+    "url": "/enegrysemulator.ml/static/js/main.c1938836.chunk.js"
+  },
+  {
+    "revision": "3f0195d6f4737829272a",
+    "url": "/enegrysemulator.ml/static/js/runtime-main.82d5f91c.js"
+  },
+  {
+    "revision": "b1abb878e2529cb5cb4450139844155d",
+    "url": "/enegrysemulator.ml/static/media/AvenirLTStd-Black.b1abb878.otf"
+  },
+  {
+    "revision": "ecb0c2ae369ba2a89d9a1ec2a1b3187b",
+    "url": "/enegrysemulator.ml/static/media/AvenirLTStd-Book.ecb0c2ae.otf"
+  },
+  {
+    "revision": "b1d7c6e085a31e9f5e4745c9aef6eb4b",
+    "url": "/enegrysemulator.ml/static/media/AvenirLTStd-Roman.b1d7c6e0.otf"
+  },
+  {
+    "revision": "fea5e4d7870ebc13e618bd786174d2ff",
+    "url": "/enegrysemulator.ml/static/media/bg.fea5e4d7.png"
+  },
+  {
+    "revision": "98d2f97305ab25d4511982e73740922e",
+    "url": "/enegrysemulator.ml/static/media/futuralightbt.98d2f973.ttf"
+  },
+  {
+    "revision": "ee64fb9d3f1ba2333e1b489283925bce",
+    "url": "/enegrysemulator.ml/static/media/futuramediumbt.ee64fb9d.ttf"
+  }
+]);
